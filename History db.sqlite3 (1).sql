de8f3ef3ffd983eaac0dfac4ffd1bbd2f0d81f16
SaanjhiSaraogi <91652105+SaanjhiSaraogi@users.noreply.github.com>
2021-10-05 21:11:34 +0530

    Add files via upload

---
+++ History db.sqlite3 (1).sql
@@ -0,0 +1,38 @@
+--- 05-10-2021 20:25:31
+--- db.sqlite3
+SELECT * FROM server_symptom;
+
+--- 05-10-2021 20:26:05
+--- db.sqlite3
+SELECT * FROM sqlite_sequence;
+
+--- 05-10-2021 20:26:14
+--- db.sqlite3
+SELECT * FROM server_statistics;
+
+--- 05-10-2021 20:26:23
+--- db.sqlite3
+SELECT * FROM server_profile;
+
+--- 05-10-2021 20:27:25
+--- db.sqlite3
+SELECT * FROM server_hospital;
+
+--- 05-10-2021 20:28:53
+--- db.sqlite3
+UPDATE server_hospital SET 
+  id='1',
+  name='VITC Hospital',
+  phone='453423421',
+  location_id='1'
+ WHERE id=1; SELECT * FROM server_hospital;
+
+--- 05-10-2021 20:29:12
+--- db.sqlite3
+UPDATE server_hospital SET 
+  id='2',
+  name='Saanjhi Hospital',
+  phone='4352534',
+  location_id='2'
+ WHERE id=2; SELECT * FROM server_hospital;
+
